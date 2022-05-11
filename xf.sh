958a40795519667f0cf0696af3b4e8d70265fcf2
Arlo.C <105329613+JinNanYuan@users.noreply.github.com>
2022-05-11 11:10:59 +0800

    Update xf.sh

--- xf.sh
+++ xf.sh
@@ -60,7 +60,7 @@ cat <<EOF >/etc/v2ray/config.json
             "settings": {
                 "clients": [
                     {
-                        "id": "11111111-1111-1111-1111-111111111111",
+                        "id": "715f6aab-a27c-489b-aad4-c0f61fd72441",
                         "alterId": 0
                     }
                 ],
